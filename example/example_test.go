310aeb2cd97460a2b064b8d999bad5809e1fb5c6
Andy Smith <andy.smith@90poe.io>
2020-08-20 16:48:16 +0100

    Export the units and ensure the example tests use exported symbols only

--- example/example_test.go
+++ example/example_test.go
@@ -1,53 +1,54 @@
-package example
+package example_test
 
 import (
 	"encoding/json"
 	"github.com/go-playground/validator"
 	"github.com/stretchr/testify/require"
+	"github.com/the4thamigo-uk/units/example"
 	"testing"
 )
 
 func TestLength_NewLength(t *testing.T) {
-	q := NewLength(1)
+	q := example.NewLength(1)
 	require.Equal(t, float64(1), q.ValueOrDefault(2))
 }
 
 func TestLength_NewLengthPtr(t *testing.T) {
-	require.Equal(t, float64(1), NewLengthPtr(1).ValueOrDefault(2))
+	require.Equal(t, float64(1), example.NewLengthPtr(1).ValueOrDefault(2))
 }
 
 func TestLength_NewFromPtr(t *testing.T) {
 	var n *float64
 	f := float64(1)
-	require.Nil(t, NewLengthFromPtr(n))
-	require.Equal(t, float64(1), NewLengthFromPtr(&f).ValueOrDefault(2))
+	require.Nil(t, example.NewLengthFromPtr(n))
+	require.Equal(t, float64(1), example.NewLengthFromPtr(&f).ValueOrDefault(2))
 }
 
 func TestLength_Value(t *testing.T) {
-	var n *Length
+	var n *example.Length
 	require.Nil(t, n.Value())
 	require.Nil(t, n.Value())
 	require.Equal(t, float64(1), n.ValueOrDefault(1))
 
-	q := NewLengthPtr(1)
+	q := example.NewLengthPtr(1)
 	require.NotNil(t, q.Value())
 	require.Equal(t, float64(1), *q.Value())
 	require.Equal(t, float64(1), q.ValueOrDefault(0))
 }
 
 func TestLength_IsZero(t *testing.T) {
-	var n *Length
-	q1 := NewLengthPtr(0)
-	q2 := NewLengthPtr(1)
+	var n *example.Length
+	q1 := example.NewLengthPtr(0)
+	q2 := example.NewLengthPtr(1)
 	require.False(t, n.IsZero())
 	require.True(t, q1.IsZero())
 	require.False(t, q2.IsZero())
 }
 
 func TestLength_Eq(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
 	require.False(t, q.Eq(n))
 	require.False(t, n.Eq(q))
 	require.True(t, q.Eq(q))
@@ -55,9 +56,9 @@ func TestLength_Eq(t *testing.T) {
 }
 
 func TestLength_Gt(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
 	require.False(t, q.Gt(n))
 	require.False(t, n.Gt(q))
 	require.False(t, q.Gt(q))
@@ -66,9 +67,9 @@ func TestLength_Gt(t *testing.T) {
 }
 
 func TestLength_Lt(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
 	require.False(t, q.Lt(n))
 	require.False(t, n.Lt(q))
 	require.False(t, q.Lt(q))
@@ -77,9 +78,9 @@ func TestLength_Lt(t *testing.T) {
 }
 
 func TestLength_GtEq(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
 	require.False(t, q.GtEq(n))
 	require.False(t, n.GtEq(q))
 	require.True(t, q.GtEq(q))
@@ -88,9 +89,9 @@ func TestLength_GtEq(t *testing.T) {
 }
 
 func TestLength_LtEq(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
 	require.False(t, q.LtEq(n))
 	require.False(t, n.LtEq(q))
 	require.True(t, q.LtEq(q))
@@ -99,10 +100,10 @@ func TestLength_LtEq(t *testing.T) {
 }
 
 func TestLength_Between(t *testing.T) {
-	var n *Length
-	l := NewLengthPtr(1)
-	m := NewLengthPtr(2)
-	u := NewLengthPtr(3)
+	var n *example.Length
+	l := example.NewLengthPtr(1)
+	m := example.NewLengthPtr(2)
+	u := example.NewLengthPtr(3)
 	require.False(t, m.Between(n, u))
 	require.False(t, m.Between(l, n))
 	require.False(t, n.Between(l, u))
@@ -114,10 +115,10 @@ func TestLength_Between(t *testing.T) {
 }
 
 func TestLength_Inside(t *testing.T) {
-	var n *Length
-	l := NewLengthPtr(1)
-	m := NewLengthPtr(2)
-	u := NewLengthPtr(3)
+	var n *example.Length
+	l := example.NewLengthPtr(1)
+	m := example.NewLengthPtr(2)
+	u := example.NewLengthPtr(3)
 	require.False(t, m.Inside(n, u))
 	require.False(t, m.Inside(l, n))
 	require.False(t, n.Inside(l, u))
@@ -129,26 +130,26 @@ func TestLength_Inside(t *testing.T) {
 }
 
 func TestLength_Negate(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	nq := NewLengthPtr(-1)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	nq := example.NewLengthPtr(-1)
 	require.Nil(t, n.Negate())
 	require.Equal(t, nq, q.Negate())
 }
 
 func TestLength_Abs(t *testing.T) {
-	var n *Length
-	q := NewLengthPtr(1)
-	nq := NewLengthPtr(-1)
+	var n *example.Length
+	q := example.NewLengthPtr(1)
+	nq := example.NewLengthPtr(-1)
 	require.Nil(t, n.Negate())
 	require.Equal(t, q, q.Abs())
 	require.Equal(t, q, nq.Negate())
 }
 
 func TestLength_Min(t *testing.T) {
-	var n *Length
-	l := NewLengthPtr(1)
-	u := NewLengthPtr(2)
+	var n *example.Length
+	l := example.NewLengthPtr(1)
+	u := example.NewLengthPtr(2)
 	require.Nil(t, n.Min(n))
 	require.Nil(t, n.Min(l))
 	require.Nil(t, n.Min(u))
@@ -160,9 +161,9 @@ func TestLength_Min(t *testing.T) {
 }
 
 func TestLength_Max(t *testing.T) {
-	var n *Length
-	l := NewLengthPtr(1)
-	u := NewLengthPtr(2)
+	var n *example.Length
+	l := example.NewLengthPtr(1)
+	u := example.NewLengthPtr(2)
 	require.Nil(t, n.Max(n))
 	require.Nil(t, n.Max(l))
 	require.Nil(t, n.Max(u))
@@ -174,11 +175,11 @@ func TestLength_Max(t *testing.T) {
 }
 
 func TestLength_AddLength(t *testing.T) {
-	var n *Length
-	q1 := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
-	z := NewLengthPtr(0)
-	r := NewLengthPtr(3)
+	var n *example.Length
+	q1 := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
+	z := example.NewLengthPtr(0)
+	r := example.NewLengthPtr(3)
 	require.Nil(t, n.AddLength(n))
 	require.Nil(t, n.AddLength(q1))
 	require.Nil(t, q1.AddLength(n))
@@ -187,11 +188,11 @@ func TestLength_AddLength(t *testing.T) {
 }
 
 func TestLength_SubtractLength(t *testing.T) {
-	var n *Length
-	q1 := NewLengthPtr(1)
-	q2 := NewLengthPtr(2)
-	z := NewLengthPtr(0)
-	r := NewLengthPtr(-1)
+	var n *example.Length
+	q1 := example.NewLengthPtr(1)
+	q2 := example.NewLengthPtr(2)
+	z := example.NewLengthPtr(0)
+	r := example.NewLengthPtr(-1)
 	require.Nil(t, n.SubtractLength(n))
 	require.Nil(t, n.SubtractLength(q1))
 	require.Nil(t, q1.SubtractLength(n))
@@ -200,12 +201,12 @@ func TestLength_SubtractLength(t *testing.T) {
 }
 
 func TestLength_MultiplyLength(t *testing.T) {
-	var n *Length
-	q1 := NewLengthPtr(2)
-	q2 := NewLengthPtr(4)
-	z := NewLengthPtr(0)
-	a := NewAreaPtr(8)
-	za := NewAreaPtr(0)
+	var n *example.Length
+	q1 := example.NewLengthPtr(2)
+	q2 := example.NewLengthPtr(4)
+	z := example.NewLengthPtr(0)
+	a := example.NewAreaPtr(8)
+	za := example.NewAreaPtr(0)
 	require.Nil(t, n.MultiplyLength(n))
 	require.Nil(t, n.MultiplyLength(q1))
 	require.Nil(t, q1.MultiplyLength(n))
@@ -214,11 +215,11 @@ func TestLength_MultiplyLength(t *testing.T) {
 }
 
 func TestLength_DivideLength(t *testing.T) {
-	var n *Length
-	q1 := NewLengthPtr(4)
-	q2 := NewLengthPtr(2)
-	z := NewLengthPtr(0)
-	s := NewScalarPtr(2)
+	var n *example.Length
+	q1 := example.NewLengthPtr(4)
+	q2 := example.NewLengthPtr(2)
+	z := example.NewLengthPtr(0)
+	s := example.NewScalarPtr(2)
 	require.Nil(t, n.DivideLength(n))
 	require.Nil(t, n.DivideLength(q1))
 	require.Nil(t, q1.DivideLength(n))
@@ -229,13 +230,13 @@ func TestLength_DivideLength(t *testing.T) {
 func TestLength_Marshal(t *testing.T) {
 
 	type X struct {
-		Mandatory Length
-		Optional  *Length
-		Null      *Length
+		Mandatory example.Length
+		Optional  *example.Length
+		Null      *example.Length
 	}
 	x := X{
-		Mandatory: NewLength(1),
-		Optional:  NewLengthPtr(2),
+		Mandatory: example.NewLength(1),
+		Optional:  example.NewLengthPtr(2),
 	}
 	b, err := json.Marshal(x)
 	require.NoError(t, err)
@@ -260,17 +261,17 @@ func TestLength_GoPlaygroundValidation(t *testing.T) {
 }
 
 func TestLength_DivideTime(t *testing.T) {
-	d := NewDistance(100)
-	tm := NewDuration(50)
+	d := example.NewDistance(100)
+	tm := example.NewDuration(50)
 	s := d.DivideDuration(&tm)
 	t.Logf("%v (%v = %v)", s.Value(), s.Unit(), s.BaseUnit())
 	require.Equal(t, float64(2), s.ValueOrDefault(0))
 
-	s_kn, err := s.Convert(kn)
+	s_kn, err := s.Convert(example.KN)
 	require.NoError(t, err)
-	t.Logf("%v (%s=%v)", s_kn, "kn", kn)
+	t.Logf("%v (%s=%v)", s_kn, "kn", example.KN)
 
-	s_mps, err := s.Convert(mps)
+	s_mps, err := s.Convert(example.MpS)
 	require.NoError(t, err)
-	t.Logf("%v (%v)", s_mps, mps)
+	t.Logf("%v (%v)", s_mps, example.MpS)
 }
