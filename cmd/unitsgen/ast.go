c0168ac80bffe5cfd68be00d63acc383bab5970e
Andy Smith <andy.smith@90poe.io>
2020-08-24 11:57:08 +0100

    Add line and column number to config validation errors

--- cmd/unitsgen/ast.go
+++ cmd/unitsgen/ast.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/alecthomas/participle/lexer"
 	"github.com/the4thamigo-uk/units"
 )
 
 type (
 	AST struct {
+		Pos        lexer.Position
+		Tok        lexer.Token
 		Package    string                 `"package" @Ident ";"`
 		Units      []*UnitDefinition      `"unit" "(" { @@ ";" } ")"`
 		Quantities []*QuantityDefinition  `"quantity" "(" { @@ ";" } ")"`
@@ -13,26 +16,36 @@ type (
 	}
 
 	UnitDefinition struct {
+		Pos         lexer.Position
+		Tok         lexer.Token
 		DerivedUnit *DerivedUnitDefinition `@@ |`
 		BaseUnit    *BaseUnitDefinition    `@@`
 	}
 
 	BaseUnitDefinition struct {
+		Pos  lexer.Position
+		Tok  lexer.Token
 		Name string `@Ident`
 	}
 
 	DerivedUnitDefinition struct {
+		Pos        lexer.Position
+		Tok        lexer.Token
 		Name       string            `@Ident "="`
 		Expression *units.Expression `@@`
 	}
 
 	QuantityDefinition struct {
+		Pos            lexer.Position
+		Tok            lexer.Token
 		Name           string           `@Ident`
 		UnitExpression units.Expression `"(" @@? ")"`
 		BaseType       string           `@("int" | "int8" | "int16" | "int32" | "int64" | "uint" | "uint8" | "uint16" | "uint32" | "uint64" |"float32" | "float64")`
 	}
 
 	OperationDefinition struct {
+		Pos      lexer.Position
+		Tok      lexer.Token
 		Result   string `@Ident "="`
 		Left     string `@Ident`
 		Operator string `@("*" | "/" | "+" | "-" | "%")`
